9a3b74ed19ecd3d994058a160b774700b70670c9
DheaJuliani <dheajuli4171@gmail.com>
2022-10-28 23:12:02 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import {useEffect, useState} from "react";
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import NotFound from "./pages/404.jsx";
-import FileProcessing from "./pages/FileProcessing";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Protected from "./components/Protected";
-import Register from "./components/Register";
-import Header from "./components/Header";
-import axios from 'axios';
-import Movie from "./components/Movie";
-import Youtube from 'react-youtube';
-import { GoogleOAuthProvider } from "@react-oauth-google";
+import {useEffect, useState} from "react"
+import './App.css'
+import axios from 'axios'
+import Movie from "./components/Movie"
+import Youtube from 'react-youtube'
 
 function App() {
     const MOVIE_API = "https://api.themoviedb.org/3/"
@@ -28,9 +16,6 @@ function App() {
     const [movies, setMovies] = useState([])
     const [searchKey, setSearchKey] = useState("")
     const [movie, setMovie] = useState({title: "Loading Movies"})
-
-    const tokenLocalStorage = localStorage.getItem("token");
-    const [token, setToken] = useState(tokenLocalStorage);
 
     useEffect(() => {
         fetchMovies()
@@ -153,32 +138,7 @@ function App() {
                 </main>
                 : 'Sorry, no movies found'}
         </div>
-
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-        <BrowserRouter>
-            <Header setToken={setToken} token={token} />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route
-                    path="/posts"
-                    element={
-            <Protected token={token} setToken={setToken}>
-                <Posts />
-            </Protected>
-            }
-        />
-            <Route path="/file-processing" element={<FileProcessing />} />
-            <Route path="/login" element={<Login token={token} setToken={setToken} />} />
-            <Route path="/register" element={<Register token={token} setToken={setToken} />} />
-
-            {/* 404 Page */}
-            <Route path="*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
-        </GoogleOAuthProvider>
     );
-    
 }
 
 export default App;
--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -7,32 +7,6 @@ function Protected({ children, token, setToken }) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Long function
-    // async function authorize() {
-    //   if (token) {
-    //     try {
-    //       // Authorize from backend
-    //       await axios.get(
-    //         "process.env.REACT_APP_AUTH_API/api/v1/auth/me",
-    //         {
-    //           headers: {
-    //             Authorization: `Bearer ${token}`,
-    //           },
-    //         }
-    //       );
-    //     } catch (error) {
-    //       if (error.response.status === 401) {
-    //         // remove token
-    //         localStorage.removeItem("token");
-    //         // Redirect to login page
-    //         navigate("/login");
-    //       }
-    //     }
-    //   }
-    // }
-    // authorize();
-    //
-    // Short function
     (async () => {
       if (token) {
         try {
